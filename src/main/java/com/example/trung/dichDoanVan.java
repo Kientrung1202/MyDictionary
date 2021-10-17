3a4e6103efc49b7ad18885dc002d5af0cc21d2bb
nmng108 <80725916+nmng108@users.noreply.github.com>
2021-10-18 00:11:47 +0700

    Update 6: translate more
    Update new functionality to translate a phrase, a sentence or paragraph by calling google translate api.

--- src/main/java/com/example/trung/dichDoanVan.java
+++ src/main/java/com/example/trung/dichDoanVan.java
@@ -1,6 +1,7 @@
 package com.example.trung;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -8,9 +9,14 @@ import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+
+import org.json.JSONArray;
 
 public class dichDoanVan {
-
     @FXML
     private ResourceBundle resources;
 
@@ -24,16 +30,27 @@ public class dichDoanVan {
     private TextArea input;
 
     @FXML
-    private ImageView btnOutput;
+    private TextArea output;
 
     @FXML
-    private TextArea output;
+    private ImageView btnOutput;
 
     @FXML
     private ImageView btnback;
 
     @FXML
-    void back(MouseEvent event) throws IOException {
+    private Button translateButton;
+
+    @FXML
+    void process() throws Exception {
+        String translatedText = input.getText();
+        if (translatedText.isBlank()) return;
+        String result = callURLAndParseResult("en", "vi", translatedText);
+        output.setText(result);
+    }
+
+    @FXML
+    void back() throws IOException {
         DictionaryApplication.setRoot("Home");
     }
 
@@ -48,16 +65,44 @@ public class dichDoanVan {
     }
 
     @FXML
-    void initialize() {
+    void initialize() throws IOException {
         assert btnInput != null : "fx:id=\"btnInput\" was not injected: check your FXML file 'dichDoanVan.fxml'.";
         assert input != null : "fx:id=\"input\" was not injected: check your FXML file 'dichDoanVan.fxml'.";
         assert btnOutput != null : "fx:id=\"btnOutput\" was not injected: check your FXML file 'dichDoanVan.fxml'.";
         assert output != null : "fx:id=\"output\" was not injected: check your FXML file 'dichDoanVan.fxml'.";
         assert btnback != null : "fx:id=\"btnback\" was not injected: check your FXML file 'dichDoanVan.fxml'.";
-
     }
 
-    void getTranslatedString() {
-        if (input.getText() != null) output.setText("Da duoc dich");
+    private String callURLAndParseResult(String langFrom, String langTo, String word) throws Exception {
+        String urlToString = "https://translate.googleapis.com/translate_a/single?"
+                + "client=gtx&"
+                + "sl=" + langFrom
+                + "&tl=" + langTo
+                + "&dt=t&q=" + URLEncoder.encode(word, "UTF-8");
+
+        URL url = new URL(urlToString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        connection.disconnect();
+        return parseResult(response.toString());
+    }
+
+    private String parseResult(String inputJson) throws Exception
+    {
+        JSONArray jsonArray = new JSONArray(inputJson);
+        JSONArray jsonArray2 = (JSONArray) jsonArray.get(0);
+        JSONArray jsonArray3 = (JSONArray) jsonArray2.get(0);
+
+        return jsonArray3.get(0).toString();
     }
 }
